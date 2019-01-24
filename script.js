47a3e0483b46a33e919fcea5197f48e571caa95b
trinanumana <31747072+trinanumana@users.noreply.github.com>
2019-01-24 13:51:22 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeImg() {
+	document.getElementById('marina').src="image/marina2.jpg";
+	document.getElementById('marinaa').src="image/marina2.jpg";
+}
+
+function changeAnothaImg() {
+	document.getElementById('marina').src="image/diamonds.png";
+	document.getElementById('marinaa').src="image/diamonds.png";
+}

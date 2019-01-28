deeb3111ef1015850a28fe71965f3d1a4936fcb3
trinanumana <31747072+trinanumana@users.noreply.github.com>
2019-01-28 14:24:28 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ function changeImg() {
 }
 
 function changeAnothaImg() {
+	document.getElementById('marina').src="image/marina4.jpg";
+	document.getElementById('marinaa').src="image/marina4.jpg";
+}
+
+function nameShow(e) {
 	document.getElementById('marina').src="image/diamonds.png";
 	document.getElementById('marinaa').src="image/diamonds.png";
+	document.getElementById('redd').classList.toggle('liteblue');
+	document.getElementById('reddd').classList.toggle('liteblue');
 }
